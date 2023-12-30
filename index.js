53a2142eda2949f50c0fc15a9be8c648348ec87b
Serge Arseneault <jirtar@gmail.com>
2023-12-29 19:03:18 -0500

    change a lot
    Change load state to message/info
    Add speed control on game pause menu
    Change msg function to a class
    the image was a bad idea, will probably remove it later (was for buff, when ball move over it. 3 buff (e.g. star) permit destroy a lightning which suppose to partially destroy the shield arrow the heart. Taking the heart end the stage. <= NOT DONE.

--- index.js
+++ index.js
@@ -6,19 +6,22 @@ function rng(min,max){
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-var canvas = document.getElementById("canvas");
+var canvas = document.createElement("canvas");//document.getElementById("canvas");
+document.body.append(canvas);
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 canvas.ctx = canvas.getContext("2d");
-console.log("use arrow keys or WSAD to move the ball. Esc to pause/stop the game loop. Relaunch it with Esc");
-console.log("Press g to be influenced by bad energy"); // you will be mind controlled by a ghost. But not totally, you are strong, you resist.
 var fps = 1000/60; // 60 fps
-var gameLoopOn = true;
+var gameLoopOn = false;
 
+/*
 document.addEventListener("contextmenu", (e)=>{
     e.preventDefault();
-    // could ad stuff here later
+    // could add stuff here later
 }, true);
+*/
+
+/*
 document.addEventListener("keypress", (e) =>{ // keypress don't work with some key, like shift, ctrl
     switch(e.key){
 
@@ -27,6 +30,7 @@ document.addEventListener("keypress", (e) =>{ // keypress don't work with some k
         }
     }
 }, true);
+*/
 
 document.addEventListener("keydown", (e) =>{ 
     switch(e.key){
@@ -54,7 +58,10 @@ document.addEventListener("keydown", (e) =>{
             gameLoopOn = !gameLoopOn; // toggle/switch between true and false. How i can forget how to do that for Christ sake.
             if(gameLoop && !gameLoopId){
                 gameLoopId = setInterval(gameLoop, fps);
-                MsgWin.remove();
+                window.msg.remove();
+            }
+            else{
+                window.msg.show();
             }
             break;
         }
@@ -63,11 +70,11 @@ document.addEventListener("keydown", (e) =>{
             if(window.ghostDuration){ break;} // or strange thing happen when more than one ghost try to take control!!!
             ball.speed = ball.speed / 2;
             window.ghostDuration = rng(60,180); // une a trois sec
-            window.ghostId = setInterval((millisecs) => {mindControled(millisecs)}, fps);
+            window.ghostId = setInterval(mindControled, fps);
             break;
         }
         default: {
-            console.log(e.key, gameLoopId);
+            //console.log(e.key, gameLoopId);
         }
     }
 }, true);
@@ -100,26 +107,48 @@ document.addEventListener("keyup", (e) =>{
     }
 }, true);
 
-function showMsg(){
-    var d = document.createElement("div");
-    d.innerHTML = "Game Paused";
-    d.style.fontSize = "24px";
-    document.body.append(d);
-    var width = document.body.clientWidth / 4;
-    var height = d.getBoundingClientRect().height;
-    d.style.width = width + "px";
-    //d.style.height = height + "px";
-    d.style.left = (document.body.clientWidth /2) - width / 2 + "px";
-    d.style.top = (document.body.clientHeight /2 ) - height / 2 + "px";
-    d.style.position = "absolute";
-    d.style.textAlign = "center";
-    d.remove = function Remove(){
-        document.body.removeChild(d);
-    };
-    window.MsgWin = d;
+class ShowMsg{
+    constructor(title){
+    this.label = document.createElement("label");
+    this.label.setAttribute("for", "speed");
+    this.label.setAttribute("id", "speed-label");
+    this.label.innerText = "Speed of the ball: ";
+    this.numberField = document.createElement("input");
+    this.numberField.setAttribute("type", "number");
+    this.numberField.setAttribute("id", "speed");
+    this.numberField.value = 10;
+    this.numberField.addEventListener("change", (e) => {ball.changeSpeed(e);}, false);
+    this.wr = document.createElement("div");
+    this.wr.setAttribute("id", "wr");
+    document.body.append(this.wr);
+    this.d = document.createElement("div");
+    this.d.innerHTML = "Game Paused";
+    this.d.style.paddingTop = "10px";
+    this.info = document.createElement("p");
+    this.info.innerText = "Press 'ESC' to show that menu/pause the game. \nPress 'WSAD' keys or Arrow key to move the ball. \nPress 'g' to make the ball possesed for a short time! \n The ball warp around the browser edge.";
+    this.d.append(this.info, this.label, this.numberField);
+    this.d.style.fontSize = "24px";
+    document.body.append(this.d);
+    this.width = document.body.clientWidth / 2;
+    this.height = this.d.getBoundingClientRect().height;
+    this.d.style.width = this.width + "px";
+    this.d.style.left = (document.body.clientWidth / 2) - this.width / 2 + "px";
+    this.d.style.top = (document.body.clientHeight / 2 ) - this.height / 2 + "px";
+    this.d.style.position = "absolute";
+    this.d.style.textAlign = "center";
+    this.d.style.backgroundColor = "#ffffff";
+    }
+    remove(){
+        this.wr.style.display = "none";
+        this.d.style.display = "none";
+    }
+    show(){
+        this.wr.style.display = "block";
+        this.d.style.display = "block";
+    }
 }
 
-var frameBuffer = [];
+var frameBuffer = []; // not in use yet, code commented and incomplete
 var gameLoopId = setInterval(gameLoop, fps);
 
 // what to draw
@@ -134,39 +163,42 @@ class Ball{
         this.moveLeft = false;
         this.moveRight = false;
         this.speed = 10; // need to tweak that
-
-        this.move = function Move(){ // for now wrap aroud edge
-            this.coordHasChange = false;
-            if(this.moveUp){
-                this.y + this.speed < 0? this.y = document.body.clientHeight: this.y -= this.speed;
-                this.coordHasChange = true;
-            }
-            if(this.moveDown){
-                this.y + this.speed > document.body.clientHeight? this.y = 0 : this.y += this.speed;
-                this.coordHasChange = true;
-            }
-            if(this.moveRight){
-                this.x + this.speed > document.body.clientWidth? this.x = 0: this.x += this.speed;
-                this.coordHasChange = true;
-            }
-            if(this.moveLeft){
-                this.x + this.speed < 0? this.x = document.body.clientWidth : this.x -= this.speed;
-                this.coordHasChange = true;
-            }
-            if(this.coordHasChange){
-                //frameBuffer.push(this);
-            }
-        };
-
-        this.draw = function Draw(){
-            this.ctx.fillStyle = "green";
-            this.ctx.beginPath();
-            this.ctx.arc(this.x, this.y, this.radius, 0, 360);
-            this.ctx.fill();
-        };
+    }
+    move(){ // for now wrap aroud edge
+        this.coordHasChange = false;
+        if(this.moveUp){
+            this.y + this.speed < 0? this.y = document.body.clientHeight: this.y -= this.speed;
+            this.coordHasChange = true;
+        }
+        if(this.moveDown){
+            this.y + this.speed > document.body.clientHeight? this.y = 0 : this.y += this.speed;
+            this.coordHasChange = true;
+        }
+        if(this.moveRight){
+            this.x + this.speed > document.body.clientWidth? this.x = 0: this.x += this.speed;
+            this.coordHasChange = true;
+        }
+        if(this.moveLeft){
+            this.x + this.speed < 0? this.x = document.body.clientWidth : this.x -= this.speed;
+            this.coordHasChange = true;
+        }
+        if(this.coordHasChange){
+            //frameBuffer.push(this);
+        }
+    }
+    draw(){
+        this.ctx.fillStyle = "green";
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, 360);
+        this.ctx.fill();
+    }
+    changeSpeed(e){
+        this.speed = parseInt(e.target.value, 10); // numberfield return string??? WTF 
     }
 }// end class Ball
 var ball = new Ball(20);
+window.msg = new ShowMsg("Game Pause");
+window.msg.show();
 
 function mindControled(){
     if (window.ghostDuration > 0){
@@ -183,22 +215,22 @@ function mindControled(){
 }
 
 function gameLoop(){
-    canvas.ctx.fillStyle = "white";
+    //canvas.ctx.fillStyle = "white";
     canvas.ctx.clearRect(0,0,canvas.width, canvas.height);
     if(gameLoopOn){
         // update canvas
         // draw frame
         ball.move();
-        ball.draw(); 
+        ball.draw();
         for(let i = 0; i < frameBuffer.length; i++){
             //frameBuffer[i].draw();
-            // append in that what ever else. Ex.: ennemi, wall ... buffSpeed, ...
+            // TODO append what ever else. Ex.: ennemi, wall ... buffSpeed, ...
         }
     }
     else{
         clearInterval(gameLoopId);
         gameLoopId = null;
-        showMsg("GamePause");
+        window.msg.show();
     }
     return; // for now want a way to stop infinite loop if happen, and stop de gameLoop ... need to rethink that
 }
